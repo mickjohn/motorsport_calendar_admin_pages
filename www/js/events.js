8318f7a22beffe3d5c6d350206c03879c9938648
Michael Ashe <mickjohnashe@hotmail.com>
2019-04-12 21:52:37 +0100

    making styles consistent

--- www/js/events.js
+++ www/js/events.js
@@ -1,7 +1,7 @@
 function init() {
   $('.event-row').click(function (e) {
     if ($(window).width() <= 700) {
-      var event_id = e.currentTarget.childNodes[1].textContent;
+      var event_id = e.currentTarget.childNodes[1].textContent.trim();
       window.location.href = `/events/${event_id}`;
     }
   });
